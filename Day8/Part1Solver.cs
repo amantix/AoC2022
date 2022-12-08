1caf9963825de18d8eb63fe8b7998226acdd7a62
Антон Марченко <18327152+amantix@users.noreply.github.com>
2022-12-08 12:04:18 +0300

    Straightforward day 8 solution

---
+++ Day8/Part1Solver.cs
@@ -0,0 +1,97 @@
+﻿using Common;
+
+namespace Day8;
+
+public class Part1Solver : ISolver<int>
+{
+    public int Solve(string[] lines)
+    {
+        var visited = new int[lines.Length, lines[0].Length];
+        for (var i = 0; i < lines.Length; i++)
+        {
+            visited[i, 0] = 1;
+            visited[i, lines[0].Length - 1] = 1;
+        }
+
+        for (var j = 0; j < lines[0].Length; j++)
+        {
+            visited[0, j] = 1;
+            visited[lines.Length - 1, j] = 1;
+        }
+
+        //left-right
+        for (var i = 1; i < lines.Length - 1; i++)
+        {
+            var max = lines[i][0];
+            for (var j = 1; j < lines[i].Length - 1; j++)
+            {
+                if (lines[i][j] > max)
+                {
+                    visited[i, j] = 1;
+                }
+
+                max = lines[i][j] > max ? lines[i][j] : max;
+            }
+        }
+
+        //right-left
+        for (var i = 1; i < lines.Length - 1; i++)
+        {
+            var max = lines[i][lines[i].Length - 1];
+            for (var j = lines[i].Length - 2; j > 0; j--)
+            {
+                if (lines[i][j] > max)
+                {
+                    visited[i, j] = 1;
+                }
+
+                max = lines[i][j] > max ? lines[i][j] : max;
+            }
+        }
+
+        //top-down
+        for (var j = 1; j < lines[0].Length - 1; j++)
+        {
+            var max = lines[0][j];
+            for (var i = 1; i < lines.Length - 1; i++)
+            {
+                if (lines[i][j] > max)
+                {
+                    visited[i, j] = 1;
+                }
+
+                max = lines[i][j] > max ? lines[i][j] : max;
+            }
+        }
+
+        //bottom-up
+        for (var j = 1; j < lines[0].Length - 1; j++)
+        {
+            var max = lines[^1][j];
+            for (var i = lines.Length - 2; i > 0; i--)
+            {
+                if (lines[i][j] > max)
+                {
+                    visited[i, j] = 1;
+                }
+
+                max = lines[i][j] > max ? lines[i][j] : max;
+            }
+        }
+
+
+        var count = 0;
+        for (var i = 0; i < lines.Length; i++)
+        {
+            for (var j = 0; j < lines[i].Length; j++)
+            {
+                if (visited[i, j] == 1)
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
---
+++ Day8/Part2Solver.cs
@@ -0,0 +1,53 @@
+﻿using Common;
+
+namespace Day8;
+
+public class Part2Solver : ISolver<int>
+{
+    public int Solve(string[] lines)
+    {
+        return Enumerable.Range(1, lines.Length - 1)
+            .SelectMany(i => Enumerable.Range(1, lines[0].Length - 1).Select(j => (i, j)))
+            .Max(coordinates => Enum.GetValues<Directions>()
+                .Select(direction => CountScenicScore(coordinates.i, coordinates.j, direction))
+                .Aggregate(1, (acc, x) => acc * x));
+
+        int CountScenicScore(int i, int j, Directions direction)
+        {
+            var count = 0;
+            var limit =
+                direction switch
+                {
+                    Directions.Right => lines[0].Length,
+                    Directions.Down => lines.Length,
+                    _ => -1
+                };
+            var start = direction switch
+            {
+                Directions.Down => i + 1,
+                Directions.Up => i - 1,
+                Directions.Left => j - 1,
+                Directions.Right => j + 1
+            };
+            var increment = direction is Directions.Down or Directions.Right ? 1 : -1;
+
+            for (var k = start; k != limit; k += increment)
+            {
+                count++;
+                if ((direction is Directions.Up or Directions.Down && lines[i][j] <= lines[k][j])
+                    || (direction is Directions.Left or Directions.Right && lines[i][j] <= lines[i][k]))
+                    break;
+            }
+
+            return count;
+        }
+    }
+
+    enum Directions
+    {
+        Down,
+        Up,
+        Left,
+        Right
+    }
+}
